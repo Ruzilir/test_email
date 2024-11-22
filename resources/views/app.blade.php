73b6f5656b003ab8013660099b3af8bff8b3e6c2
Ruzilir <ruzilzrp@mail.ru>
2024-11-22 12:36:26 +0500

    scss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
     @inertiaHead
     @routes
   </head>
